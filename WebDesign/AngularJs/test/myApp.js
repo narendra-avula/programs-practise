2f3b3dda1844b71cf23d53f2b36f3bd363b0ad67
narendradivi <narendraavula2@gmail.com>
2016-09-09 13:59:17 +0530

    angular js

---
+++ WebDesign/AngularJs/test/myApp.js
@@ -0,0 +1,5 @@
+/**
+ * Created by narendra on 2/9/16.
+ */
+
+var app = angular.module('myApp', []);
---
+++ WebDesign/AngularJs/test/myCtrl.js
@@ -0,0 +1,8 @@
+/**
+ * Created by narendra on 2/9/16.
+ */
+
+app.controller('myCtrl', function ($scope) {
+    $scope.firstName = 'Naidu';
+    $scope.lastName = 'Narendra';
+});
